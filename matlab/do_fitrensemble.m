7a253425022097100bcb7ae0d0b204aab632d08a
troyraen <troy.raen@pitt.edu>
2019-04-02 08:39:48 -0400

    update fitrensemble params

--- matlab/do_fitrensemble.m
+++ matlab/do_fitrensemble.m
@@ -4,8 +4,8 @@ function [mdl, res, mse, test_res] = do_fitrensemble(data, target, test_data)
 % target should be 1 x Nexamples with correct answer
 % test_data is matrix of test data
 
-mdl = fitrensemble(data,target, 'OptimizeHyperparameters','auto');
-
+% mdl = fitrensemble(data,target, 'OptimizeHyperparameters','auto');
+mdl = fitrensemble(data,target, 'Method','Bag', 'MinLeafSize',1);
 
 res = predict(mdl, data);
 
