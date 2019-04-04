7928bf48ed4316ec8f152e50d212e85fa3f94283
troyraen <troy.raen@pitt.edu>
2019-04-03 23:44:23 -0400

    calc_zerrors() now takes abs_dev as input. function abs_dev added.

--- matlab/calc_zerrors.m
+++ matlab/calc_zerrors.m
@@ -1,12 +1,9 @@
-function [NMAD, out10] = calc_zerrors(specz, photz)
+function [NMAD, out10] = calc_zerrors(abs_dev)
 % zspec is true redshift
 % zphot is predicted redshift
 % both should be column vectors
 % Returns normalized median absolute deviation (NMAD)
 %           and fraction of 10% outliers, where NAD > 0.1 (out10)
 
-N = length(specz);
-
-NAD = abs(photz - specz)./(1+specz);
-NMAD = 1.48* median(NAD);
-out10 = sum(NAD > 0.1)/N;
+NMAD = 1.48* median(abs_dev);
+out10 = sum(abs_dev > 0.1)/length(abs_dev);
