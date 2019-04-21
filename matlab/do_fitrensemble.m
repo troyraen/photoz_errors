18a19fd7f72cb2a2d135dd78c8216709fad562dc
troyraen <troy.raen@pitt.edu>
2019-04-20 23:07:28 -0400

    update do_fitrensemble

--- matlab/do_fitrensemble.m
+++ matlab/do_fitrensemble.m
@@ -1,13 +1,15 @@
-function [mdl, res, mse, test_res] = do_fitrensemble(data, target, test_data)
+function [mdl, res, mse, test_res] = do_fitrensemble(data, target, test_data, params)
 % Builds a random forest.
 % data should be matrix (rows=examples, columns=attributes)
 % target should be 1 x Nexamples with correct answer
 % test_data is matrix of test data
+% params is parameters for fitrensemble, = {NumLearningCycles}
 
 % mdl = fitrensemble(data,target, 'OptimizeHyperparameters','auto');
-mdl = fitrensemble(data,target, 'Method','Bag');
+mdl = fitrensemble(data,target, 'Method','Bag', 'NumLearningCycles',params{1})
 
-res = predict(mdl, data);
+res = predict(mdl, data); % takes an average over trees fit in previous step
+% https://www.mathworks.com/help/stats/ensemble-algorithms.html#bsw8at7
 
 test_res = predict(mdl, test_data);
 
